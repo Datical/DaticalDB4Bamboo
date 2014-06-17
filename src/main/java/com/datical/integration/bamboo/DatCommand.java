35c512ad24d21813d40419ec7fcf727ae0215c16
r2datical <r2@datical.com>
2014-06-17 15:09:17 -0500

    added commands, sql export

---
+++ src/main/java/com/datical/integration/bamboo/DatCommand.java
@@ -0,0 +1,29 @@
+package com.datical.integration.bamboo;
+
+public class DatCommand {
+
+	private String command;
+	private String description;
+
+	public DatCommand(String command, String description) {
+
+		new DatCommand(command, description);
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
